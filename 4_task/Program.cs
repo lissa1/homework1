4c91c6c7e3a97fe8b3f62d937b65738f542bac11
lissa <e-namestnikova@inbox.ru>
2022-07-02 12:02:25 +0700

    add fourth task

---
+++ 4_task/Program.cs
@@ -0,0 +1,5 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("ввeдите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 2; i <= n; i = i + 2)
+    Console.WriteLine(i);
